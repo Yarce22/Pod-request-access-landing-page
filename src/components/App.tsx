22ba859ed2cd33d5e508e081c0cb241aec867748
Yarce22 <alejomira96@gmail.com>
2024-03-15 14:15:21 -0500

    Se corrige error de despliegue del background

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,12 +1,14 @@
 import Logo from './Logo'
 import MobileBody from './MobileBody';
+import host from '../assets/mobile/image-host.jpg';
 import '../styles/App.css'
 
 function App() {
   return (
     <>
-      <section className='w-full h-[100vh] bg-background-mb bg-cover'>
-        <div className='w-full h-[100vh] px-12 bg-background opacity-90'>
+      <section className='relative w-full h-[100vh] bg-cover'>
+        <img className="absolute w-full h-[100vh] z-[-1]" src={host} alt="image-host" />
+        <div className='absolute w-full h-[100vh] px-12 bg-background opacity-90'>
           <header className='flex items-center justify-center w-full h-60'>
             <Logo />
           </header>
