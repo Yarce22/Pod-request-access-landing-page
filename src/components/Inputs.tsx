4879b5f91869e09a03116e1096be13b89df78507
Yarce22 <alejomira96@gmail.com>
2024-03-15 19:51:33 -0500

    Se termina todo el diseño mobile

--- src/components/Inputs.tsx
+++ src/components/Inputs.tsx
@@ -1,11 +1,40 @@
+import React from "react";
+import { useState } from "react";
+
 const Inputs = () => {
+  const [isAEmail, setEmailValue] = useState(true);
+  const [inputValue, setInputValue] = useState('');
+
+  const handleText = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const input = event.target.value;
+    setInputValue(input);
+  }
+
+  const isInputValueAEmail = () => {
+    const value = inputValue;
+    const valueString = value.includes('@') && value.includes('.');
+    setEmailValue(valueString);
+  }
+
   return (
     <>
-      <form className="flex flex-col justify-center items-center w-full py-10 gap-5">
+      <form className="flex flex-col justify-center items-center w-full py-10 gap-8">
         <label className="w-full" htmlFor="">
-          <input className="w-full h-12 px-8 bg-input opacity-100 rounded-full font-bold text-sm text-clear border-none outline-none" type="email"  placeholder={'Email address'}/>
+          <input 
+            className="w-full h-12 px-8 bg-input opacity-100 rounded-full font-bold text-sm text-clear border-none outline-none"
+            type="email"
+            placeholder={'Email address'}
+            onChange={handleText}
+          />
+          <span className={`${isAEmail ? 'hidden' : 'block'} pl-8 text-xs text-red-600 font-bold`}>Opps! Please check your email</span>
         </label>
-        <button className="w-full h-12 rounded-full text-base font-bold text-background bg-title hover:bg-hover">Request Access</button>
+        <button
+          className="w-full h-12 rounded-full text-base font-bold text-background bg-title hover:bg-hover"
+          type="button"
+          onClick={isInputValueAEmail}
+        >
+          Request Access
+        </button>
       </form>
     </>
   );
